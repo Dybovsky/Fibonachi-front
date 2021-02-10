73a60c8b798822390fd2863c19c94a0149db8a58
Tom <a.dybovsky@gmail.com>
2021-02-10 15:38:33 +0200

    4 mil

--- js/index.js
+++ js/index.js
@@ -1,29 +1,31 @@
-function find_fib(n) {
-  let prev = 0,
-    next = 1;
-  for (let i = 0; i < n; i++) {
-    let temp = next;
-    next = prev + next;
-    prev = temp;
-  }
-  return prev;
-}
+// function find_fib(n) {
+//   let prev = 0,
+//     next = 1;
+//   for (let i = 0; i < n; i++) {
+//     let temp = next;
+//     next = prev + next;
+//     prev = temp;
+//   }
+//   return prev;
+// }
 
 // function find_fib(n) {
 //   const a = (1 + 5 ** 0.5) / 2;
 //   return Math.round(a ** n / 5 ** 0.5);
 // }
+
 let calculate_btn = document.getElementById("calculate");
 let userInput = document.getElementById("user_input");
-let userNum = 0;
-let answer;
+let newp = document.getElementById("answer");
+
 calculate_btn.addEventListener("click", () => {
-  userNum = userInput.value;
-
-  answer = find_fib(userNum);
-
-  newp.innerText = `${answer}`;
+  callServer(`http://localhost:5050/fibonacci/${userInput.value}`);
 });
 
-// Creating elements
-let newp = document.getElementById("answer");
+function callServer(link) {
+  fetch(link).then((response) => {
+    response.json().then((data) => {
+      newp.innerText = data.result;
+    });
+  });
+}
