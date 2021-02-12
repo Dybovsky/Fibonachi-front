64b9d10c1dda52fb2badfdd3d85ba659a14e5741
Tom <a.dybovsky@gmail.com>
2021-02-12 20:22:49 +0200

    5 mil

--- js/index.js
+++ js/index.js
@@ -16,16 +16,69 @@
 
 let calculate_btn = document.getElementById("calculate");
 let userInput = document.getElementById("user_input");
-let newp = document.getElementById("answer");
+let answer = document.getElementById("answer");
+let spinner = document.getElementById("spinner");
+let url = "";
+let alert = document.getElementById("alert");
 
-calculate_btn.addEventListener("click", () => {
-  callServer(`http://localhost:5050/fibonacci/${userInput.value}`);
-});
+function gettingResult() {
+  url = `http://localhost:5050/fibonacci/${userInput.value}`;
+  console.log(userInput.value);
+  if (userInput.value > 50) {
+    showError("Can`t be larger than 50");
+    //userInput.value = "";
+  } else {
+    callServer(url);
+  }
+}
 
 function callServer(link) {
+  displaySpinner();
   fetch(link).then((response) => {
-    response.json().then((data) => {
-      newp.innerText = data.result;
-    });
+    if (response.ok) {
+      response.json().then((data) => {
+        addResInTag(data);
+      });
+    } else {
+      showError(response.statusText);
+    }
+    hideSpinner();
   });
 }
+
+function addResInTag(data) {
+  answer.innerText = data.result;
+}
+
+function displaySpinner() {
+  spinner.style.display = "block";
+}
+
+function hideSpinner() {
+  spinner.style.display = "none";
+}
+
+function showError(textError) {
+  userInput.classList.add("text-danger");
+  // , "is-invalid");
+  alert.style.display = "block";
+  alert.style.opacity = "1";
+  answer.innerText = "";
+  alert.innerText = textError;
+  userInput.style.borderColor = "red";
+}
+
+function hideError() {
+  userInput.classList.remove("text-danger");
+  // , "is-invalid");
+  alert.style.opacity = "0";
+  answer.innerText = "";
+  userInput.style.removeProperty("border-color");
+}
+calculate_btn.addEventListener("click", () => {
+  gettingResult();
+});
+
+calculate_btn.addEventListener("blur", () => {
+  hideError();
+});
