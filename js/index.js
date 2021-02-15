d40840020dceeb8fdb0c035d4ba311d44809f522
Tom <a.dybovsky@gmail.com>
2021-02-15 09:21:48 +0200

    6 mile

--- js/index.js
+++ js/index.js
@@ -18,8 +18,11 @@ let calculate_btn = document.getElementById("calculate");
 let userInput = document.getElementById("user_input");
 let answer = document.getElementById("answer");
 let spinner = document.getElementById("spinner");
+let spinner2 = document.getElementById("spinner2");
 let url = "";
 let alert = document.getElementById("alert");
+let resultsUrl = "http://localhost:5050/getFibonacciResults";
+let resultsUl = document.getElementById("results");
 
 function gettingResult() {
   url = `http://localhost:5050/fibonacci/${userInput.value}`;
@@ -33,7 +36,7 @@ function gettingResult() {
 }
 
 function callServer(link) {
-  displaySpinner();
+  displaySpinner(spinner);
   fetch(link).then((response) => {
     if (response.ok) {
       response.json().then((data) => {
@@ -42,7 +45,7 @@ function callServer(link) {
     } else {
       showError(response.statusText);
     }
-    hideSpinner();
+    hideSpinner(spinner);
   });
 }
 
@@ -50,17 +53,16 @@ function addResInTag(data) {
   answer.innerText = data.result;
 }
 
-function displaySpinner() {
-  spinner.style.display = "block";
+function displaySpinner(name) {
+  name.style.display = "block";
 }
 
-function hideSpinner() {
-  spinner.style.display = "none";
+function hideSpinner(name) {
+  name.style.display = "none";
 }
 
 function showError(textError) {
   userInput.classList.add("text-danger");
-  // , "is-invalid");
   alert.style.display = "block";
   alert.style.opacity = "1";
   answer.innerText = "";
@@ -70,15 +72,41 @@ function showError(textError) {
 
 function hideError() {
   userInput.classList.remove("text-danger");
-  // , "is-invalid");
+  // , "is-invalid";
   alert.style.opacity = "0";
   answer.innerText = "";
   userInput.style.removeProperty("border-color");
 }
+
+function displayResults(obj) {
+  const historyItem = document.createElement("li");
+  const resDate = new Date(obj.createdDate);
+  historyItem.innerHTML = `The Fibonacci of <strong>${obj.number}</strong>
+   is <strong>${obj.result}</strong>. Calculated at: ${resDate} `;
+  resultsUl.appendChild(historyItem);
+}
+
+function callList(url) {
+  displaySpinner(spinner2);
+  fetch(url).then((response) => {
+    response.json().then((data) => {
+      for (let item of data.results) {
+        displayResults(item); // console.log(item);  answer <-
+      }
+    });
+    hideSpinner(spinner2);
+  });
+}
+
 calculate_btn.addEventListener("click", () => {
   gettingResult();
+  callList(resultsUrl);
 });
 
 calculate_btn.addEventListener("blur", () => {
   hideError();
 });
+
+window.onload = function () {
+  callList(resultsUrl);
+};
